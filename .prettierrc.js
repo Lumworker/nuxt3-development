9fc8db3dd1b190b11b95601f59fc064d4676cded
@chonprakun <chonprakun@riverpark.co.th>
2023-11-18 11:58:31 +0700

    add;prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  semi: true,
+  bracketSpacing: true,
+};
