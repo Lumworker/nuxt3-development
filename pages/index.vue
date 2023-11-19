834430b9954f4cea9246f1a0f5c35f0028f592be
@chonprakun <chonprakun@riverpark.co.th>
2023-11-20 00:23:00 +0700

    add table

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,35 @@
     <add-ticket-dialog :get-ticket-store-function="getTicketStore" />
     <add-transaction-modal :dialog="isModalOpen" :close-modal-transaction="closeModalTransaction" />
 
-
+    <template>
+      <v-table fixed-header height="300px">
+        <thead>
+          <tr>
+            <th class="text-left">
+              buyerName
+            </th>
+            <th class="text-left">
+              buyerDate
+            </th>
+            <th class="text-left">
+              ticketType
+            </th>
+            <th class="text-left">
+              amout
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in ticketTransaction" :key="item.id">
+            <td>{{ item.buyerName }}</td>
+            <td>{{ item.buyerDate }}</td>
+            <td>{{ item.ticketType }}</td>
+            <td>{{ item.amout }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </template>
+    <!-- {{ ticketTransaction }} -->
   </v-app>
 </template>
 
@@ -26,9 +54,11 @@ import AddTransactionModal from '@/components/AddTransactionModal.vue';
 
 onMounted(async () => {
   await getTicketStore();
+  await getTicketTransaction();
 
   onBeforeUnmount(() => {
     getTicketStore();
+    getTicketTransaction();
   });
 });
 
@@ -42,6 +72,7 @@ interface TicketType {
 ;
 
 const ticketStore = ref<TicketType[]>([]);
+const ticketTransaction = ref<TicketType[]>([]);
 const getTicketStore = async (): Promise<any> => {
   try {
     const response = await $fetch("/api/query?col=ticketStore");
@@ -49,6 +80,21 @@ const getTicketStore = async (): Promise<any> => {
 
     if (result) {
       ticketStore.value = result.slice().sort((a, b) => a.ticketType.localeCompare(b.ticketType));
+    } else {
+      console.error("Invalid API response format");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+const getTicketTransaction = async (): Promise<any> => {
+  try {
+    const response = await $fetch("/api/query?col=ticketTransaction");
+    const { result } = response as { result: any[] };
+
+    if (result) {
+      ticketTransaction.value = result;
     } else {
       console.error("Invalid API response format");
     }
